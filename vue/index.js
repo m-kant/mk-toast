1fdfffb448c1ca9f97465899106994f67d2266f6
kantemirov <mihail.kantemirov@prk-a.ru>
2018-03-28 12:11:33 +0300

    vue connector from es6 to es5

--- vue/index.js
+++ vue/index.js
@@ -1,6 +1,6 @@
-/** bridge to Vue.js for easy usage in components 
+/** bridge to Vue.js for easy usage in components
  *  without multiply imports in every file.
- *  In App.vue in style segment add: 
+ *  In App.vue in style segment add:
  *    @import "../node_modules/mk-toast/dist/mk-toast.css";
  *  In main.js add:
  *    import mktoast from 'mk-toast/vue';
@@ -9,17 +9,17 @@
  *    this.$toast.echo(message)
  */
 
-import mktoast from '../dist/mk-toast';
-// import mktoastStyles from './mk-toast-styles';
+var mktoast = require('../dist/mk-toast') ;
 
-const Toast = {
-
-  install(Vue, toastDefaults = {}) {
+var Toast = {
+  /* pure ES6 works well in Dev mode, but leads to error during Build.
+     so had to change func definition from "func(){}" to "func: function(){}"
+     had to remove default argument value "func(a=10)"
+  */
+  install: function(Vue, toastDefaults){
     Object.assign(mktoast.defaults, toastDefaults);
-    // Vue.component('mktoastStyles', mktoastStyles);
     Vue.prototype.$mktoast = mktoast;
   }
-
 };
 
-export default Toast;
+module.exports = Toast;
